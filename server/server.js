f5ce1c4ca83663694c6ed4f48bf6377a2c8594d1
Mike de Vries <mbv.spike@gmail.com>
2025-04-14 20:52:20 +0200

    added news feature
    - now loads image from weird non-parsed json html
    - parser JSDOM added

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import { App } from '@tinyhttp/app';
 import { logger } from '@tinyhttp/logger';
 import { Liquid } from 'liquidjs';
 import sirv from 'sirv';
-
+import { JSDOM } from 'jsdom';
 
 const engine = new Liquid({
   extname: '.liquid',
@@ -21,7 +21,6 @@ app
   .use('/', sirv(process.env.NODE_ENV === 'development' ? 'client' : 'dist'))
   .listen(3000, () => console.log('Server available on http://localhost:3000'));
 
-
 app.get('/', async (req, res) => {
   // HIER GAMES OPHALEN
   // const url = `https://api.steampowered.com/ISteamNews/GetNewsForApp/v2/?appid=${gameNr}&count=3`
@@ -37,42 +36,43 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-app.get('card/:appid', async (req, res) => {
-  const appId = req.params.appid;
-  // url FOR NEWS ITEMS ATTACHED TO A GAME
-  const url = `https://api.steampowered.com/ISteamNews/GetNewsForApp/v2/?appid=${appId}&count=12`
+// app.get('card/:appid', async (req, res) => {
+//   const appId = req.params.appid;
+//   // url FOR NEWS ITEMS ATTACHED TO A GAME
+//   const url = `https://api.steampowered.com/ISteamNews/GetNewsForApp/v2/?appid=${appId}&count=12`
 
-  // urlDetails FOR A GAME WITH SIMPLIFIED GAME DETAILS LIKE TAGS/GENRES
-  const urlDetails = `https://steamspy.com/api.php?request=appdetails&appid=${appId}`;
+//   // urlDetails FOR A GAME WITH SIMPLIFIED GAME DETAILS LIKE TAGS/GENRES
+//   const urlDetails = `https://steamspy.com/api.php?request=appdetails&appid=${appId}`;
 
-  // urlGameDeepDetails FOR A GAME WITH ALL THE DETAILS
-  const urlGameDeepDetails = `https://store.steampowered.com/api/appdetails?appids=${appId}`;
+//   // urlGameDeepDetails FOR A GAME WITH ALL THE DETAILS
+//   const urlGameDeepDetails = `https://store.steampowered.com/api/appdetails?appids=${appId}`;
 
-  const gameDetails = await fetch(urlDetails);
-  const gameDetailsData = await gameDetails.json();
+//   const gameDetails = await fetch(urlDetails);
+//   const gameDetailsData = await gameDetails.json();
 
-  const gameNews = await fetch(url);
-  const gameNewsData = await gameNews.json();
+//   const gameNews = await fetch(url);
+//   const gameNewsData = await gameNews.json();
 
 
-  const gameDeepDetails = await fetch(urlGameDeepDetails);
-  const gameDeepDetailsData = await gameDeepDetails.json();
+//   const gameDeepDetails = await fetch(urlGameDeepDetails);
+//   const gameDeepDetailsData = await gameDeepDetails.json();
 
-  const gameImageLength = gameDeepDetailsData.data.screenshots.length;
-  const screenshots = gameDeepDetailsData.data.screenshots;
-  const randomImage = screenshots[getRandomInt(gameImageLength - 1)];
+//   const gameImageLength = gameDeepDetailsData.data.screenshots.length;
+//   const screenshots = gameDeepDetailsData.data.screenshots;
+//   const randomImage = screenshots[getRandomInt(gameImageLength - 1)];
 
-  console.log(gameNewsData);
-  console.log(gameDetailsData);
-  console.log(gameDeepDetailsData);
+//   console.log(gameNewsData);
+//   console.log(gameDetailsData);
+//   console.log(gameDeepDetailsData);
 
-  return res.send(renderTemplate('server/components/card/card.liquid', { title: 'card item' }));
-});
+//   return res.send(renderTemplate('server/components/card/card.liquid', { title: 'card item' }));
+// });
 
 app.get('game/:appid', async (req, res) => {
   const appId = req.params.appid;
+
   // url FOR NEWS ITEMS ATTACHED TO A GAME
-  const url = `https://api.steampowered.com/ISteamNews/GetNewsForApp/v2/?appid=${appId}&count=12`
+  const url = `https://api.steampowered.com/ISteamNews/GetNewsForApp/v2/?appid=${appId}&count=12`;
 
   // urlDetails FOR A GAME WITH SIMPLIFIED GAME DETAILS LIKE TAGS/GENRES
   const urlDetails = `https://steamspy.com/api.php?request=appdetails&appid=${appId}`;
@@ -84,24 +84,52 @@ app.get('game/:appid', async (req, res) => {
   const gameDetailsData = await gameDetails.json();
 
   const gameNews = await fetch(url);
-  const gameNewsData = await gameNews.json();
-
+  const gameNewsDataRaw = await gameNews.json();
 
   const gameDeepDetails = await fetch(urlGameDeepDetails);
   const gameDeepDetailsData = await gameDeepDetails.json();
 
-  const screenshots = gameDeepDetailsData[appId].data.screenshots
+  const screenshots = gameDeepDetailsData[appId].data.screenshots;
   const gameImageLength = screenshots.length;
   const randomImage = screenshots[getRandomInt(gameImageLength - 1)];
+
+  // ✅ FIX: voeg eerste img toe aan elk nieuwsitem
+  const newsItems = gameNewsDataRaw.appnews.newsitems.map(item => {
+    let firstImg = null;
+
+    try {
+      const dom = new JSDOM(item.contents);
+      const img = dom.window.document.querySelector('img');
+      if (img) firstImg = img.src;
+    } catch (err) {
+      console.warn("Fout bij het parsen van afbeelding:", err);
+    }
+
+    return {
+      ...item,
+      firstImg: firstImg
+    };
+  });
+
+  const gameNewsData = {
+    appnews: {
+      ...gameNewsDataRaw.appnews,
+      newsitems: newsItems
+    }
+  };
 
   console.log(gameNewsData);
   console.log(gameDetailsData);
   console.log(gameDeepDetailsData);
 
-  return res.send(renderTemplate('server/views/detail.liquid', { title: 'Home', gameNewsData: gameNewsData, gameDetailsData: gameDetailsData, gameDeepDetailsData: gameDeepDetailsData[appId], randomImage: randomImage }));
-}
-);
-
+  return res.send(renderTemplate('server/views/detail.liquid', {
+    title: 'Home',
+    gameNewsData: gameNewsData,
+    gameDetailsData: gameDetailsData,
+    gameDeepDetailsData: gameDeepDetailsData[appId],
+    randomImage: randomImage
+  }));
+});
 
 const renderTemplate = (template, data) => {
   const templateData = {
@@ -112,16 +140,9 @@ const renderTemplate = (template, data) => {
   return engine.renderFileSync(template, templateData);
 };
 
-
-
-
-
-
 // http://api.steampowered.com/<interface name>/<method name>/v<version>/?key=<api key>&format=<format>
 
 // https://api.steampowered.com
-
-
 
 // async function getData() {
 //   const url = "https://store.steampowered.com/api/appdetails?appids=730";
